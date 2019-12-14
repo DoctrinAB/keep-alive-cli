fd5d7c3b37c1abc382ae38d43c5877e838ea9c8a
Karl Pokus <karl.pokus@gmail.com>
2019-12-14 16:41:59 +0100

    promisify all thangs

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const fail = err => {
 	console.error('err', err.message);
 	process.exit(1);
 };
+const dumpHeaders = (req, res) => {
+	const stringify = obj => JSON.stringify(obj.headers, 2, null);
+	if (process.env.HEADERS == 1) {
+		console.log('request headers:', stringify(req));
+		console.log('response headers:', stringify(res));
+	} else {
+		console.log('response header connection:', res.headers.connection);
+	}
+};
 
 if (!uri) {
 	fail(new Error('uri is required'));
@@ -16,22 +25,12 @@ if (!delay) {
 
 const parsedURI = url.parse(uri);
 
-const dumpHeaders = (req, res) => {
-	const stringify = obj => JSON.stringify(obj.headers, 2, null);
-	if (process.env.HEADERS == 1) {
-		console.log('request headers:', stringify(req));
-		console.log('response headers:', stringify(res));
-	} else {
-		console.log('response header connection:', res.headers.connection);
-	}
-};
-
 const agent = new http.Agent({
 	keepAlive: true,
 	keepAliveMsecs: 30000 // 30s
 });
 
-const makeRequest = cb => {
+const call = n => new Promise((resolve, reject) => {
 	const req = {
 		hostname: parsedURI.hostname,
 	  port: parsedURI.port,
@@ -40,30 +39,28 @@ const makeRequest = cb => {
 		agent: agent
 	};
 
-	http.request(req, res => {
+	const cb = res => {
 		dumpHeaders(req, res);
-
 		let data = '';
 		res
-			.on('error', fail)
-			.on('data', chunk => {
-				data += chunk;
-			})
+			.on('error', reject)
+			.on('data', chunk => data += chunk)
 			.on('end', () => {
-				cb(data);
+				console.log(`request ${ n } done. response: ${ data }`);
+				resolve();
 			});
-	}).on('error', fail)
-	.end();
-};
+	};
 
-console.log('request 1 start');
-makeRequest(data => {
-	console.log('request 1 done. response:', data);
-	console.log(`time to kill the server.. you have ${ delay }ms until next request`);
-	setTimeout(() => {
-		console.log('request 2 start');
-		makeRequest(data => {
-			console.log('request 2 done. response:', data);
-		});
-	}, delay);
+	console.log(`request ${ n } start`);
+
+	http.request(req, cb)
+		.on('error', reject)
+		.end();
 });
+
+call(1)
+	.then(() => {
+		console.log(`time to kill the server.. you have ${ delay }ms until next request`);
+		return setTimeout(() => call(2), delay);
+	})
+	.catch(fail);
